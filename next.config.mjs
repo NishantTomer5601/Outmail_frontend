87acf82561d3be5eb3159bf87bea1bc2b3e6b336
Tanmay0215 <tanmaygupta.0215@gmail.com>
2026-03-16 22:35:09 +0530

    update packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: [
-      'images.unsplash.com',
-      'lh3.googleusercontent.com',  // Google profile pictures
-      'res.cloudinary.com'          // Cloudinary images (already used in your code)
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "outmail.in.s3.ap-south-1.amazonaws.com",
+      }
     ],
   },
 };
